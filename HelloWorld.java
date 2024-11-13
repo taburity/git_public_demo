4f634e55cd7e6174e5423fb9ff6490a937733de6
taburity <thais.burity@ufape.edu.br>
2024-11-13 13:11:59 -0300

    Limpando o código

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,6 @@ public class HelloWorld {
 
 	public static void main(String[] args){
 		System.out.println("Hello World!");
-		System.out.println("edição no master");
-		System.out.println("edição na nova branch");
 	}
 
 }
